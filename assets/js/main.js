de0aa524ff143e02862db9a7f402f2b805c652d2
ilias <essatenilas@gmail.com>
2023-07-19 16:41:03 +0300

    carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 window.addEventListener("scroll", function(){
     const headerNav = this.document.getElementById ("header-nav").classList.toggle("headernav-scroll", window.scrollY > 135)
 })
+
+$(document).ready(function(){
+    $(".owl-carousel-full").owlCarousel({
+        margin:20,
+        responsive:{ /* по сколько карточек на какой ширине */
+            0:{ /* от 0 до 600 будет одна карточка */
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{ /* от 600 до 1000 будет три карточки */
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+  });
